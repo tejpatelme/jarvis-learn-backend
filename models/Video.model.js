8140a9492d76fa72ceedd0ef92cce681c4f52264
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-20 10:38:00 +0530

    create : Video model

---
+++ models/Video.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const VideoSchema = new Schema({
+  imageURL: {
+    type: String,
+    required: "URL of video image is required",
+  },
+  name: {
+    type: String,
+    required: "Video name is required",
+  },
+  description: {
+    type: String,
+    required: "Description of the video is required",
+  },
+  channelName: {
+    type: String,
+    required: "Channel name is required",
+  },
+  channelLogoURL: {
+    type: String,
+    required: "URL of channel logo image is required",
+  },
+  topic: {
+    type: String,
+    required: "Topic of the video is required",
+  },
+});
+
+const Video = model("Video", VideoSchema);
+
+module.exports = { Video };
