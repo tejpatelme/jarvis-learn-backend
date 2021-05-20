d4f4006af2777b3e598d85bec3814a186a3454e7
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-20 11:44:47 +0530

    add error handler middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV === "development") {
 
 const express = require("express");
 const { connectToDatabase } = require("./db/dbConnection");
+const { errorHandler } = require("./middlewares/error-handler");
 const { routeNotFound } = require("./middlewares/route-not-found");
 
 const PORT = process.env.PORT | 3000;
@@ -15,10 +16,15 @@ app.get("/", (req, res) => {
   res.send("Backend API for jarvis learn video library app");
 });
 
-/*
+/**
  * 404 Router Handler.
  * Do not move, this needs to be the last route.
  */
 app.use(routeNotFound);
+
+/**
+ *Error Handler
+ */
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log("Server started on port ", PORT));
