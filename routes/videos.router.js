5674cec846400bf1a3926527d15904edb3ff71e7
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-20 12:32:35 +0530

    add "/" route to fetch videos

--- routes/videos.router.js
+++ routes/videos.router.js
@@ -7,7 +7,18 @@ const router = Router();
 router.post("/seed", async (req, res) => {
   try {
     const videos = await Video.create(videoLibrary);
+
     res.json({ success: true, videos });
+  } catch (err) {
+    res.status(500).json({ success: false, errorMessage: err.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const videos = await Video.find({});
+
+    res.json(videos);
   } catch (err) {
     res.status(500).json({ success: false, errorMessage: err.message });
   }
