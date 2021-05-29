5b36281546890a5fefdf16e0e8a1816f60ce82f2
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-29 16:38:46 +0530

    create : controller for users routes

---
+++ controllers/users.controller.js
@@ -0,0 +1,43 @@
+const { User } = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.signUpUser = async (req, res) => {
+  const user = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    const newUser = await User.create(user);
+
+    res.status(201).json({ success: true, newUser });
+  } catch (err) {
+    res.status(500).json({ success: false, errorMessage: err.message });
+  }
+};
+
+exports.logInUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const passwordValid = await bcrypt.compare(password, user.password);
+
+      if (passwordValid) {
+        const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
+          expiresIn: "24h",
+        });
+
+        return res.status(200).json({ token });
+      }
+    }
+
+    return res
+      .status(401)
+      .json({ success: false, errorMessage: "email or password is incorrect" });
+  } catch (err) {
+    res.status(500).json({ success: false, errorMessage: err.message });
+  }
+};
