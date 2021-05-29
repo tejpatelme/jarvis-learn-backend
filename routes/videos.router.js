0c766a7b80e6b57fa9c975d3f485da9b623fd24d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-29 16:41:00 +0530

    refactor : move functions to controller

--- routes/videos.router.js
+++ routes/videos.router.js
@@ -1,27 +1,13 @@
 const { Router } = require("express");
-const { Video } = require("../models/video.model");
-const { videoLibrary } = require("../data/video-data");
+const {
+  seedVideosDataToDatabase,
+  fetchAllVideos,
+} = require("../controllers/videos.controller");
 
 const router = Router();
 
-router.post("/seed", async (req, res) => {
-  try {
-    const videos = await Video.create(videoLibrary);
+router.post("/seed", seedVideosDataToDatabase);
 
-    res.json({ success: true, videos });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
-});
-
-router.get("/", async (req, res) => {
-  try {
-    const videos = await Video.find({});
-
-    res.json(videos);
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
-});
+router.get("/", fetchAllVideos);
 
 module.exports = router;
