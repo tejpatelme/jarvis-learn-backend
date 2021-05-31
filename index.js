c89d4b6b98773c6f0f18fb9dd32dc8da7c7a64f8
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-31 20:08:34 +0530

    refactor : dotenv config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === "development") {
-  require("dotenv").config();
-}
+// if (process.env.NODE_ENV === "development") {
 
+// }
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { connectToDatabase } = require("./db/dbConnection");
