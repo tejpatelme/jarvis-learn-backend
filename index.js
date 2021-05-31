4f413616c4257630f3a11eaffed6fc5202ad8574
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-31 22:02:26 +0530

    fix : deployment error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
-// }
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { connectToDatabase } = require("./db/dbConnection");
